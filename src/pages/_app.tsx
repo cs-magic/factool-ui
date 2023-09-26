ed6c4b286bec017e8a2d4d0b8c55d1ce507df876
markshawn2020 <shawninjuly@gmail.com>
2023-09-26 20:47:51 +0800

    added table

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,15 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Theme>
+      <Component {...pageProps} />
+    </Theme>
+  );
 };
 
 export default api.withTRPC(MyApp);
